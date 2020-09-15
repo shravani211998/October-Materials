04c8a07ccb177077a85d90d5c497d5206f7c2a77
Kishor <kishor1124@gmail.com>
2020-09-15 17:28:55 +0530

    first day materials

---
+++ Day 1/HelloApp.java
@@ -0,0 +1,17 @@
+class A {
+	public static void main(String[] args)	{
+		System.out.println("Hello World");
+	}
+}
+
+class Simple {
+	public static void main(String[] args)	{
+		System.out.println("Hello HSBC");
+	}
+}
+class Xyz {
+	void demo() {
+		
+	}
+}
+
