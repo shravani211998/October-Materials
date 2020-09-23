330d96833dcc4168b36a505fd5290ef60e0d805f
Kishor <kishor1124@gmail.com>
2020-09-23 14:49:51 +0530

    Collection Backed Dao Implementation

---
+++ user-profile-examples/src/com/hsbc/model/dao/CollectionBackedUserDao.java
@@ -0,0 +1,33 @@
+package com.hsbc.model.dao;
+
+import com.hsbc.model.beans.User;
+
+public class CollectionBackedUserDao implements UserDao {
+	
+	// create a ArrayList with List reference
+
+	@Override
+	public User store(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User[] fetchUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User fetchUserById(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User updateUser(int userId, User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
